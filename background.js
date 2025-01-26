da463e45e611f50855c756b01ddd5d31edf60324
syedtalaljilani <syedtalaljilani.dev@gmail.com>
2025-01-26 23:44:44 +0500

    background.js update

--- background.js
+++ background.js
@@ -1,6 +1,10 @@
 // background.js
-
+// Register persistent event listeners
 chrome.runtime.onInstalled.addListener(() => {
-    console.log("AI Assistant Extension installed!");
-  });
-  
+  console.log('Service worker activated');
+});
+
+// Add regular keep-alive pings
+setInterval(() => {
+  console.log('Service worker heartbeat');
+}, 30 * 1000); // 30 seconds
